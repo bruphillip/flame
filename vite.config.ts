eac58aa31c9125088a323c1f9db468ce619fb9bd
Bruno Philippe <bruni.felipe@hotmail.com>
2024-02-20 19:34:40 -0300

    feat: added tests and config

--- vite.config.ts
+++ vite.config.ts
@@ -8,44 +8,38 @@ import viteTsconfigPaths from 'vite-tsconfig-paths';
 
 // import eslint from 'vite-plugin-eslint';
 
-export default ({ mode }) => {
-  process.env = { ...process.env, ...loadEnv(mode, process.cwd()) };
-
-  return defineConfig({
-    resolve: {
-      alias: {
-        src: '/src',
-      },
+export default defineConfig({
+  resolve: {
+    alias: {
+      src: '/src',
     },
-    build: {
-      target: ['es2018'],
-      lib: {
-        entry: resolve(__dirname, 'src/index.ts'),
-        name: 'flame',
-        formats: ['es'],
-        fileName: 'flame-lib',
-      },
-      rollupOptions: {
-        external: ['react', 'react-dom'],
-        output: {
-          globals: {
-            react: 'React',
-            'react-dom': 'ReactDOM',
-          },
+  },
+  build: {
+    target: ['es2018'],
+    lib: {
+      entry: resolve(__dirname, 'src/index.ts'),
+      name: 'flame',
+      formats: ['es'],
+      fileName: 'flame-lib',
+    },
+    rollupOptions: {
+      external: ['react', 'react-dom'],
+      output: {
+        globals: {
+          react: 'React',
+          'react-dom': 'ReactDOM',
         },
       },
     },
-    plugins: [
-      react(),
-      dts({
-        // insertTypesEntry: true,
-        rollupTypes: true,
-        copyDtsFiles: true,
-      }),
-      viteTsconfigPaths(),
-      // eslint({
-      //   exclude: ['/virtual:/', 'node_modules/**'],
-      // }),
-    ],
-  });
-};
+  },
+  plugins: [
+    react(),
+    dts({
+      insertTypesEntry: true,
+    }),
+    viteTsconfigPaths(),
+    // eslint({
+    //   exclude: ['/virtual:/', 'node_modules/**'],
+    // }),
+  ],
+});
